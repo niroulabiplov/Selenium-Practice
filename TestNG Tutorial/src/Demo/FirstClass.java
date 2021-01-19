848db152a05877c45d9d89885b37201b7d4d976e
Biplov <niroula.biplov@gmail.com>
2021-01-19 16:26:46 +0545

    TestNG Practices

---
+++ TestNG Tutorial/src/Demo/FirstClass.java
@@ -0,0 +1,27 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class FirstClass {
+	@Test
+	
+	public void FirstTC(){
+		System.out.println("Inside First TC");
+	}
+	@Test
+	
+	public void SecondTC(){
+		System.out.println("Inside Second TC");
+}
+@Test
+	
+	public void ThirdTC() {
+		System.out.println("Inside third Class");
+	}
+@Test
+
+public void FourthTC() {
+	System.out.println("Inside fourth Class");
+}
+	}
+
---
+++ TestNG Tutorial/src/Demo/SecondClass.java
@@ -0,0 +1,11 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class SecondClass {
+	@Test
+	
+	public void ThirdTC() {
+		System.out.println("Inside third Class");
+	}
+}
